3bff29e7cc5f977f1d2574416c0eb5f05260959e
Ayananshu Mohanty <119516509+AyananshuMohanty@users.noreply.github.com>
2023-11-27 01:50:14 +0530

    Update Graph_Optimization.py

--- Graph_Optimization.py
+++ Graph_Optimization.py
@@ -2,16 +2,25 @@ from Course_Input import *
 from Professor_Input import *
 import networkx as nx
 
-G = nx.Graph()
+G = nx.DiGraph(directed=True)
 print(G)
 
 for course in courselist:
     G.add_node(course.getCourseCode())
+    
+pos = nx.circular_layout(G)
 
 for professor in professorList:
     G.add_node(professor.getID())
     for i in range(16):
         if i!=None:
             G.add_edge(professor.getName(),professor.getPriority(i))
+            nx.draw_networkx_edge_labels(
+                G, pos,
+                edge_labels={(professor.getName(),professor.getPriority(i)): 'Priority: ' + i},
+                font_color='red'
+            )
 
 nx.draw(G, with_labels = True)
+
+
