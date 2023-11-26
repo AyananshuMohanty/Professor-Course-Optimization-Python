ac765d9a04477cecb587a564b4531c8cd00efb01
Ayananshu Mohanty <119516509+AyananshuMohanty@users.noreply.github.com>
2023-11-27 01:30:16 +0530

    Create Graph_Optimization.py

---
+++ Graph_Optimization.py
@@ -0,0 +1,17 @@
+from Course_Input import *
+from Professor_Input import *
+import networkx as nx
+
+G = nx.Graph()
+print(G)
+
+for course in courselist:
+    G.add_node(course.getCourseCode())
+
+for professor in professorList:
+    G.add_node(professor.getID())
+    for i in range(16):
+        if i!=1:
+            G.add_edge(professor.getName(),professor.getPriority(i))
+
+nx.draw(G, with_labels = True)
