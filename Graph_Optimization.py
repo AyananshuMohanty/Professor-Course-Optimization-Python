b64c0b8af5bab23e784c3f9df4e46916914bc793
Ayananshu Mohanty <119516509+AyananshuMohanty@users.noreply.github.com>
2023-11-27 19:44:15 +0530

    Update Graph_Optimization.py
    Changes names to unique IDs

--- Graph_Optimization.py
+++ Graph_Optimization.py
@@ -13,39 +13,47 @@ for course in courselist:
     
 
 for professor in professorList:
-    G.add_node(professor.getName())
-    for i in range(1,6):
+    professorID=professor.getID()
+    G.add_node(professorID)
+    for i in range(1,len(professor.Priority_Order_FDCDC)+1):
+        courseCode=professor.getFDCDCPriority(i).getCourseCode()
         if professor.getFDCDCPriority(i)!='nan':
-            G.add_edge(professor.getName(),professor.getFDCDCPriority(i).getName())
-            # nx.draw_networkx_edge_labels(
-            #     G, pos,
-            #     edge_labels={(professor.getName(),professor.getFDCDCPriority(i).getName()): 'Priority: ' + str(i)},
-            #     font_color='red'
-            # )
-    for i in range(1,6):
+            G.add_edge(professorID,courseCode)
+            nx.draw_networkx_edge_labels(
+                G, pos,
+                edge_labels={(professorID,courseCode): 'Priority: ' + str(i)},
+                font_color='red'
+            )
+            
+    for i in range(1,len(professor.Priority_Order_HDCDC)+1):
+        courseCode=professor.getHDCDCPriority(i).getCourseCode()
         if professor.getHDCDCPriority(i)!='nan':
-            G.add_edge(professor.getName(),professor.getHDCDCPriority(i).getName())
-            # nx.draw_networkx_edge_labels(
-            #     G, pos,
-            #     edge_labels={(professor.getName(),professor.getHDCDCPriority(i).getName()): 'Priority: ' + str(i)},
-            #     font_color='red'
-            # )
-    for i in range(1,4):
+            G.add_edge(professorID,courseCode)
+            nx.draw_networkx_edge_labels(
+                G, pos,
+                edge_labels={(professorID,courseCode): 'Priority: ' + str(i)},
+                font_color='red'
+            )
+            
+    for i in range(1,len(professor.Priority_Order_FDELC)+1):
+        courseCode=professor.getFDELCPriority(i).getCourseCode()
         if professor.getFDELCPriority(i)!='nan':
-            G.add_edge(professor.getName(),professor.getFDELCPriority(i).getName())
-            # nx.draw_networkx_edge_labels(
-            #     G, pos,
-            #     edge_labels={(professor.getName(),professor.getFDELCPriority(i).getName()): 'Priority: ' + str(i)},
-            #     font_color='red'
-            # )
-    for i in range(1,4):
+            G.add_edge(professorID,courseCode)
+            nx.draw_networkx_edge_labels(
+                G, pos,
+                edge_labels={(professorID,courseCode): 'Priority: ' + str(i)},
+                font_color='red'
+            )
+            
+    for i in range(1,len(professor.Priority_Order_HDELC)+1):
+        courseCode=professor.getHDELCPriority(i).getCourseCode()
         if professor.getHDELCPriority(i)!='nan':
-            G.add_edge(professor.getName(),professor.getHDELCPriority(i).getName())
-            # nx.draw_networkx_edge_labels(
-            #     G, pos,
-            #     edge_labels={(professor.getName(),professor.getHDELCPriority(i).getName()): 'Priority: ' + str(i)},
-            #     font_color='red'
-            # )
+            G.add_edge(professorID,courseCode)
+            nx.draw_networkx_edge_labels(
+                G, pos,
+                edge_labels={(professorID,courseCode): 'Priority: ' + str(i)},
+                font_color='red'
+            )
 
 print(G)
 nx.draw(G, with_labels = True)
