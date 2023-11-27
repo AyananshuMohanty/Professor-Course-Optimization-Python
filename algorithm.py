f1555b91c0bf68d1d3168664bcb7aa646d649bcd
chaphekar <chaphekaryash@gmail.com>
2023-11-28 01:41:57 +0530

    Create algorithm.py

---
+++ algorithm.py
@@ -0,0 +1,17 @@
+from Professor_Input import *
+class Solution:
+    def assignFirstCDCPriority(self):
+        for prof in professorList:            
+            for course in prof.Priority_Order_FDCDC+prof.Priority_Order_HDCDC:
+                if(course.noOfProfsTakingCourse<2):
+                    break 
+                
+            prof.addCourseTaken(course) #need to subtract 1 or 0.5 from the prof too, need to add a field to prof
+            course.addProfTakingCourse(prof)
+            if(course.noOfProfsTakingCourse==1):
+                prof.coursesRemaining -= 1
+            elif(course.noOfProfsTakingCourse==2):
+                prof.coursesRemaining -= 0.5
+                course.profsTakingCourse[0].coursesRemaining += 0.5
+                                   
+    # def sortProfessors(self): will create this after updating input file, add x1,x2,x3 as fields
