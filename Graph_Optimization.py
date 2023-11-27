a08b0bd34b59035533393bda385d8f70345f6831
chaphekar <chaphekaryash@gmail.com>
2023-11-27 19:16:30 +0530

    Graph ban gaya

--- Graph_Optimization.py
+++ Graph_Optimization.py
@@ -16,34 +16,35 @@ for professor in professorList:
     for i in range(1,6):
         if professor.getFDCDCPriority(i)!='nan':
             G.add_edge(professor.getName(),professor.getFDCDCPriority(i).getName())
-            nx.draw_networkx_edge_labels(
-                G, pos,
-                edge_labels={(professor.getName(),professor.getFDCDCPriority(i).getName()): 'Priority: ' + str(i)},
-                font_color='red'
-            )
+            # nx.draw_networkx_edge_labels(
+            #     G, pos,
+            #     edge_labels={(professor.getName(),professor.getFDCDCPriority(i).getName()): 'Priority: ' + str(i)},
+            #     font_color='red'
+            # )
     for i in range(1,6):
         if professor.getHDCDCPriority(i)!='nan':
             G.add_edge(professor.getName(),professor.getHDCDCPriority(i).getName())
-            nx.draw_networkx_edge_labels(
-                G, pos,
-                edge_labels={(professor.getName(),professor.getHDCDCPriority(i).getName()): 'Priority: ' + str(i)},
-                font_color='red'
-            )
+            # nx.draw_networkx_edge_labels(
+            #     G, pos,
+            #     edge_labels={(professor.getName(),professor.getHDCDCPriority(i).getName()): 'Priority: ' + str(i)},
+            #     font_color='red'
+            # )
     for i in range(1,4):
         if professor.getFDELCPriority(i)!='nan':
             G.add_edge(professor.getName(),professor.getFDELCPriority(i).getName())
-            nx.draw_networkx_edge_labels(
-                G, pos,
-                edge_labels={(professor.getName(),professor.getFDELCPriority(i).getName()): 'Priority: ' + str(i)},
-                font_color='red'
-            )
+            # nx.draw_networkx_edge_labels(
+            #     G, pos,
+            #     edge_labels={(professor.getName(),professor.getFDELCPriority(i).getName()): 'Priority: ' + str(i)},
+            #     font_color='red'
+            # )
     for i in range(1,4):
         if professor.getHDELCPriority(i)!='nan':
             G.add_edge(professor.getName(),professor.getHDELCPriority(i).getName())
-            nx.draw_networkx_edge_labels(
-                G, pos,
-                edge_labels={(professor.getName(),professor.getHDELCPriority(i).getName()): 'Priority: ' + str(i)},
-                font_color='red'
-            )
+            # nx.draw_networkx_edge_labels(
+            #     G, pos,
+            #     edge_labels={(professor.getName(),professor.getHDELCPriority(i).getName()): 'Priority: ' + str(i)},
+            #     font_color='red'
+            # )
 
-nx.draw(G, with_labels = True)       
+print(G)
+# nx.draw(G, with_labels = True)       
