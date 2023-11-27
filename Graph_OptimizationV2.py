b64cbec46f9d8881899c1fea67521690f99fff4c
Ayananshu Mohanty <119516509+AyananshuMohanty@users.noreply.github.com>
2023-11-27 20:25:01 +0530

    Create Graph_OptimizationV2.py

---
+++ Graph_OptimizationV2.py
@@ -0,0 +1,58 @@
+from Course_Input import *
+from Professor_Input import *
+import networkx as nx
+import matplotlib.pyplot as plt
+plt.figure(figsize=(100, 100))
+G = nx.Graph()
+print(G)
+
+pos = nx.spring_layout(G)
+
+for course in courselist:
+    G.add_node(course.getName())
+    
+
+for professor in professorList:
+    professorID=professor.getID()
+    G.add_node(professorID)
+    for i in range(1,len(professor.Priority_Order_FDCDC)+1):
+        courseCode=professor.getFDCDCPriority(i).getCourseCode()
+        if professor.getFDCDCPriority(i)!='nan':
+            G.add_edge(professorID,courseCode)
+            
+    for i in range(1,len(professor.Priority_Order_HDCDC)+1):
+        courseCode=professor.getHDCDCPriority(i).getCourseCode()
+        if professor.getHDCDCPriority(i)!='nan':
+            G.add_edge(professorID,courseCode)
+            
+    for i in range(1,len(professor.Priority_Order_FDELC)+1):
+        courseCode=professor.getFDELCPriority(i).getCourseCode()
+        if professor.getFDELCPriority(i)!='nan':
+            G.add_edge(professorID,courseCode)
+            
+    for i in range(1,len(professor.Priority_Order_HDELC)+1):
+        courseCode=professor.getHDELCPriority(i).getCourseCode()
+        if professor.getHDELCPriority(i)!='nan':
+            G.add_edge(professorID,courseCode)
+
+print(G)
+
+edge_label_dictionary={}
+for edges in G.edges():
+    edge_label_dictionary[edges]=edges[0]
+
+# nx.draw_networkx_edge_labels(
+#     G, pos,
+#     edge_label_dictionary,
+#     font_color='red'
+# )
+
+print(G)
+options = {
+    'node_color': 'yellow',     # color of node
+    'node_size': 3500,          # size of node
+    'width': 1,                 # line width of edges  
+    'edge_color':'blue',        # edge color
+}
+nx.draw(G, pos, with_labels = True)
+plt.savefig("Graph.png")
